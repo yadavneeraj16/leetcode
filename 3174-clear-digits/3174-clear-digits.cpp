299c8c3925676ffcb18892e4386c48ef05f35403
Neeraj Kumar <122194555+yadavneeraj16@users.noreply.github.com>
2024-06-09 15:45:51 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (100.00%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     string clearDigits(string s) 
     {
-        int n=s.length();
-        string ans="";
+//         int n=s.length();
+//         string ans="";
         
-        int i=0;
-        while(i<n)
-        {
-            if(!isdigit(s[i])){
-                ans=ans+s[i];
-                i++;
-            }
-            else
-            {
-                i++;
-                if(ans.size()>=1)
-                {
-                    ans.pop_back();
-                }
-            }
-        }
-         return ans;
+//         int i=0;
+//         while(i<n)
+//         {
+//             if(!isdigit(s[i])){
+//                 ans=ans+s[i];
+//                 i++;
+//             }
+//             else
+//             {
+//                 i++;
+//                 if(ans.size()>=1)
+//                 {
+//                     ans.pop_back();
+//                 }
+//             }
+//         }
+//          return ans;
+        string res;
+    for (char ch : s)
+        if (isdigit(ch))
+            res.pop_back();
+        else
+            res += ch;
+    return res;
       }
       
     
