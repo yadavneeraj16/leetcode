2d528e8f67b01eeb371fa387973639aa03ed3c9f
Neeraj Kumar <122194555+yadavneeraj16@users.noreply.github.com>
2024-06-24 12:45:17 +0530

    Time: 360 ms (14.29%), Space: 157.4 MB (28.57%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -24,25 +24,5 @@ public:
         return (h[a-1]-h[0]+1)*(v[b-1]-v[0]+1);
     }
 };
-//    vector<int>horizontal_index;
-//        vector<int>vertical_index;
-        
-        
-//         for(int i = 0 ; i< grid.size();i++){
-            
-//             for(int j= 0 ; j < grid[0].size() ;j++){
-                
-//                if(grid[i][j]==1){
-//                    horizontal_index.push_back(j);
-//                    vertical_index.push_back(i);
-//                }
-//             }
-//         }
-        
-//         // Now we can sort the vector and find the min and max i and j for grid[i][j] = 1
-//         sort(horizontal_index.begin(),horizontal_index.end());
-//         sort(vertical_index.begin(),vertical_index.end());
-        
-//         //Finally caluculate the area of rec.
-//         return (horizontal_index[horizontal_index.size()-1]-horizontal_index[0]+1)*(vertical_index[vertical_index.size()-1]-vertical_index[0]+1);
+
         
