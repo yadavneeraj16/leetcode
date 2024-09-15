705c9b823723ca0648c66736768d5795984b0f29
Neeraj Kumar <122194555+yadavneeraj16@users.noreply.github.com>
2024-09-15 12:50:01 +0530

    Time: 12 ms (16.67%), Space: 27.4 MB (11.11%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int>ans;
+           unordered_map<int,int>mp;
+           for(int i=0;i<nums.size();i++)
+           {
+            mp[nums[i]]++;
+           }
+           for(auto it:mp){
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+           }return ans;
+    }
+};
