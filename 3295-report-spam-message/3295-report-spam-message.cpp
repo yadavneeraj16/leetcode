1f67d90a87d2f9cfc7de2e593a0ef8e1514a4e86
Neeraj Kumar <122194555+yadavneeraj16@users.noreply.github.com>
2024-09-24 10:40:24 +0530

    Time: 1100 ms (6.12%), Space: 346.4 MB (17.16%) - LeetHub

--- 3295-report-spam-message/3295-report-spam-message.cpp
+++ 3295-report-spam-message/3295-report-spam-message.cpp
@@ -3,24 +3,15 @@ public:
     bool reportSpam(vector<string>& v, vector<string>& b) 
     {
         map<string,int>mp;
-        set<string>st;
-        set<string>bt;
-        int n=v.size();int c=0;
-        int m=b.size();
-        // for(int i=0;i<n;i++){
-        //     st.insert(v[i]);
-        // }
-//         for(int i=0;i<m;i++)
-//         {
-//             bt.insert(b[i]);
-           
-//         }
+        
+        int c=0;
+       
         for(auto it:b){
             mp[it]++;
         }
           for(auto itt:v)
         {
-          // mp[itt]++;
+          
             
             if(mp[itt]>=1)
             {
